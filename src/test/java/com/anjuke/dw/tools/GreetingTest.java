1ffd8206cf84f7fc2b0b1ecbdac82d149269161a
Ji ZHANG <zhangji87@gmail.com>
2014-08-17 23:23:44 +0800

    init

---
+++ src/test/java/com/anjuke/dw/tools/GreetingTest.java
@@ -0,0 +1,39 @@
+package com.anjuke.dw.tools;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class GreetingTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void greeting() throws Exception {
+        this.mockMvc.perform(get("/greeting").accept(MediaType.parseMediaType("text/html;charset=utf-8")))
+            .andExpect(status().isOk());
+    }
+
+}
