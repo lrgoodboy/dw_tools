22e43c017532d559e04d5aef2b4509e2bbcc1f60
Ji ZHANG <jizhang@anjuke.com>
2014-08-24 23:48:18 +0000

    encoding issue

--- src/main/java/com/anjuke/dw/tools/Application.java
+++ src/main/java/com/anjuke/dw/tools/Application.java
@@ -1,8 +1,12 @@
 package com.anjuke.dw.tools;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -10,6 +14,14 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
     }
 
 }
