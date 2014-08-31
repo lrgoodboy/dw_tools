cf1a28d4a11dc6abacdfd62de5941b13b26383d8
Ji ZHANG <jizhang@anjuke.com>
2014-08-31 08:47:09 +0000

    login by cookie

--- src/main/java/com/anjuke/dw/tools/controller/LoginController.java
+++ src/main/java/com/anjuke/dw/tools/controller/LoginController.java
@@ -3,6 +3,13 @@ package com.anjuke.dw.tools.controller;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -12,6 +19,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.apache.tomcat.util.buf.HexUtils;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +28,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -35,7 +44,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SessionAttributes("user")
 public class LoginController {
 
-    public static final String DEFAULT_FROM = "/";
+    private static final String DEFAULT_FROM = "/";
+    private static final String COOKIE_NAME = "dw_tools_auth";
+    private static final Integer COOKIE_EXPIRE = 7 * 24 * 3600;
+    private static final String HMAC_KEY = "dwrocks";
+    private static final String HMAC_ALGORITHM = "HmacMD5";
+
     private Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Value("${oauth.url}")
@@ -51,14 +65,46 @@ public class LoginController {
     private ObjectMapper json;
 
     @RequestMapping("/login")
-    public View login(@RequestParam(value = "access_token", required = false) String accessToken,
+    public View login(@CookieValue(value = COOKIE_NAME, required = false) String cookieToken,
+            @RequestParam(value = "access_token", required = false) String accessToken,
             @RequestParam(value = "from", defaultValue = DEFAULT_FROM) String from,
             @RequestParam(value = "custom", defaultValue = DEFAULT_FROM) String custom,
-            Model model) throws Exception {
+            Model model, HttpServletResponse response) throws Exception {
+
+        User user = null;
+
+        if (cookieToken != null) {
+            try {
+                user = loginByCookie(cookieToken);
+            } catch (Exception e) {
+                logger.info("Login by cookie failed, remove the cookie.", e);
+                response.addCookie(new Cookie(COOKIE_NAME, null));
+            }
+
+            if (user != null) {
+                logger.info("Login by cookie: " + user.getUsername());
+                model.addAttribute("user", user);
+                return redirect(from);
+            }
+        }
 
         if (accessToken != null) {
-            model.addAttribute("user", loginUser(accessToken));
-            return redirect(custom);
+            try {
+                user = loginByAuth(accessToken);
+            } catch (Exception e) {
+                logger.info("Login by auth failed.", e);
+            }
+
+            if (user != null) {
+                logger.info("Login by auth.");
+                model.addAttribute("user", user);
+
+                Cookie cookie = new Cookie(COOKIE_NAME, getCookieToken(user.getId()));
+                cookie.setMaxAge(COOKIE_EXPIRE);
+                response.addCookie(cookie);
+
+                return redirect(custom);
+            }
         }
 
         return redirect(getAuthUrl(from));
@@ -69,7 +115,7 @@ public class LoginController {
         redirect.setStatusCode(HttpStatus.FOUND);
         redirect.setExposeModelAttributes(false);
         redirect.setUrl(url);
-        logger.info("redirect: " + url);
+        logger.info("Redirect: " + url);
         return redirect;
     }
 
@@ -82,11 +128,11 @@ public class LoginController {
                 nvps.add(new BasicNameValuePair(params[i], params[i + 1]));
             }
             httpPost.setEntity(new UrlEncodedFormEntity(nvps));
-            logger.info(String.format("request json, url: %s, params: %s", url, nvps));
+            logger.info(String.format("Request json, url: %s, params: %s", url, nvps));
             CloseableHttpResponse response = httpClient.execute(httpPost);
             try {
                 JSONObject info = json.readValue(EntityUtils.toString(response.getEntity()), JSONObject.class);
-                logger.info("result: " + info.toString());
+                logger.info("Result: " + info.toString());
                 return info;
             } finally {
                 response.close();
@@ -96,7 +142,7 @@ public class LoginController {
         }
     }
 
-    private User loginUser(String accessToken) throws Exception {
+    private User loginByAuth(String accessToken) throws Exception {
 
         JSONObject info = requestJson(oauthUrl + "/resource.php",
                 "oauth_token", accessToken,
@@ -145,6 +191,31 @@ public class LoginController {
                 .queryParam("code", getTemporaryToken())
                 .queryParam("custom", from)
                 .build().toUriString();
+    }
+
+    private User loginByCookie(String cookieToken) throws Exception {
+
+        Pattern ptrn = Pattern.compile("^([0-9]+)_(.*)$");
+        Matcher matcher = ptrn.matcher(cookieToken);
+        if (!matcher.matches()) {
+            throw new Exception("Invalid token format.");
+        }
+
+        if (!matcher.group(2).equals(hmac(matcher.group(1)))) {
+            throw new Exception("Invalid token.");
+        }
+
+        return userRepository.findOne(Long.parseLong(matcher.group(1)));
+    }
+
+    private String getCookieToken(long userId) throws Exception {
+        return String.format("%d_%s", userId, hmac(String.valueOf(userId)));
+    }
+
+    public String hmac(String input) throws Exception {
+        Mac mac = Mac.getInstance(HMAC_ALGORITHM);
+        mac.init(new SecretKeySpec(HMAC_KEY.getBytes(), HMAC_ALGORITHM));
+        return HexUtils.toHexString(mac.doFinal(input.getBytes()));
     }
 
 }
