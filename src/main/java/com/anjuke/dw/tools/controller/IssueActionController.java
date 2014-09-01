430df699d70d513cdd6f50688b48c00db98a81ce
Ji ZHANG <jizhang@anjuke.com>
2014-09-01 19:42:09 +0800

    update reply count

--- src/main/java/com/anjuke/dw/tools/controller/IssueActionController.java
+++ src/main/java/com/anjuke/dw/tools/controller/IssueActionController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.anjuke.dw.tools.dao.IssueActionRepository;
+import com.anjuke.dw.tools.dao.IssueRepository;
+import com.anjuke.dw.tools.model.Issue;
 import com.anjuke.dw.tools.model.IssueAction;
 
 
@@ -17,9 +19,16 @@ public class IssueActionController {
 
     @Autowired
     private IssueActionRepository issueActionRepository;
+    @Autowired
+    private IssueRepository issueRepository;
 
     @RequestMapping("delete/{actionId}")
     public void delete(@PathVariable("actionId") IssueAction action) {
+
+        Issue issue = issueRepository.findOne(action.getIssueId());
+        issue.setReplyCount(issue.getReplyCount() - 1);
+        issueRepository.save(issue);
+
         issueActionRepository.delete(action);
     }
 
